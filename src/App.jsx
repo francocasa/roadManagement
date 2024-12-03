fafe76c3d7588048ea05c14ff90d2a74c8954b4d
Franco <20141674@aloe.ulima.edu.pe>
2024-12-02 23:42:55 -0500

    feat: login y logout con Auth0

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,15 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<ProfilePage />} />
+        </Routes>
+      </Router>
     </>
   );
 }
